130f928c2ed3bc1b72d29c40d0bc93c161968ec0
Amit Khayargoli <khayargoliamit99@gmail.com>
2025-12-27 15:42:05 +0545

    Refactor Hero and Navbar components for improved styling and responsiveness

--- client/src/app/_components/Hero.tsx
+++ client/src/app/_components/Hero.tsx
@@ -5,21 +5,33 @@ interface PillDescProps {
 }
 function PillDesc({ desc }: PillDescProps) {
   return (
-    <div className="glass bg-[rgba(0, 54, 0, 0.2)] border border-[#375506] px-4 py-2 rounded-full w-fit mb-4 hover:bg-white/20 cursor-pointer">
-      <span className="text-[16px]">{desc}</span>
-    </div>
+    <>
+      {/* <div className="bg-[rgba(0, 54, 0, 0.2)] border border-[#375506] px-4 py-2 rounded-full w-fit mb-4 hover:bg-white/20 cursor-pointer">
+        <span className="text-[16px]">{desc}</span>
+      </div> */}
+
+      <div
+        className="bg-[#003600]/20 backdrop-blur-md 
+            border border-[#375506] rounded-full
+            shadow-2xl transition-all duration-300 hover:bg-[#003600]/40 
+            w-fit mb-4 cursor-pointer px-4 py-2"
+      >
+        <span className="text-white text-[16px]">{desc}</span>
+      </div>
+    </>
   );
 }
 
 export default function Hero() {
   return (
-    <div className="w-full flex flex-col items-center py-20">
+    <div className=" relative w-full flex flex-col items-center py-14">
       {/* pill shaped hero desc*/}
       <PillDesc desc="📜✨ Set your life in Motion with AI Notes  " />
 
       <h1 className="text-5xl font-bold text-center text-[#D2FF89] mb-14">
         All-In-One Collaboration and <br /> Productivity Platform
       </h1>
+
       <Image src="/Workspace.png" alt={""} width={900} height={400}></Image>
 
       {/* logos */}
