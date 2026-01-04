c0d886abeff8a23c696f4bb6e36c152046a69d8f
Amit Khayargoli <khayargoliamit99@gmail.com>
2026-01-04 21:31:33 +0545

    Added user dtos and types

---
+++ server/src/dtos/user.dto.ts
@@ -0,0 +1,24 @@
+import { UserSchema } from "../types/user.type";
+import z, { email } from "zod";
+
+export const CreateUserDTO = UserSchema.pick({
+  email: true,
+  password: true,
+  username: true,
+})
+  .extend({
+    confirmPassword: z.string().min(6),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
+
+export const LoginUserDTO = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
