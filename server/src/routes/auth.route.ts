2a9bc2749acd4d209c9653ad116ec786aa8a99ce
Amit Khayargoli <khayargoliamit99@gmail.com>
2026-02-09 23:16:06 +0545

    Added routes for audioFiles, user, notes and workspaces

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authorizedMiddleware } from "../middleware/authorized.middleware";
+import { uploads } from "../middleware/file-upload";
 
 const router = Router();
 const authController = new AuthController();
@@ -7,4 +9,11 @@ const authController = new AuthController();
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/users", authController.getAllUsers);
+router.put(
+  "/user/update",
+  authorizedMiddleware,
+  uploads.single("image"),
+  authController.updateProfile,
+);
+
 export default router;
