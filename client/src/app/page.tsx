19333b9a15d54b9cffae3112c0960e0a1a276eff
Amit Khayargoli <khayargoliamit99@gmail.com>
2026-03-03 19:11:02 +0545

    feat: add Error, Loading, and NotFound components for improved user experience and error handling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,25 +1,13 @@
 "use client";
 
-import LoginModal from "./(auth)/_components/LoginModal";
-import SignupModal from "./(auth)/_components/SignupModal";
 import Hero from "./_components/Hero";
 import Navbar from "./_components/Navbar";
-import { useAuthStore } from "@/store/auth.store";
 
 export default function Home() {
-  const {
-    isLoginModalOpen,
-    isSignupModalOpen,
-    closeLoginModal,
-    closeSignupModal,
-  } = useAuthStore();
   return (
     <div className="min-h-screen bg-home-gradient">
       <Navbar />
       <Hero />
-
-      <LoginModal isOpen={isLoginModalOpen} onClose={closeLoginModal} />
-      <SignupModal isOpen={isSignupModalOpen} onClose={closeSignupModal} />
     </div>
   );
 }
